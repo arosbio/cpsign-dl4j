5976398103b01e34c0cbfebab26221f11e637da8
staffan <staffan.arvidsson@gmail.com>
2021-11-15 20:40:51 +0100

    update param name testSplit -> testFrac

--- src/main/java/com/arosbio/ml/dl4j/DL4JMultiLayerBase.java
+++ src/main/java/com/arosbio/ml/dl4j/DL4JMultiLayerBase.java
@@ -314,8 +314,7 @@ public abstract class DL4JMultiLayerBase
 	private static List<String> N_EPOCH_CONF_NAMES = Arrays.asList("nEpoch", "numEpoch");
 	private static List<String> BATCH_SIZE_CONF_NAMES = Arrays.asList("batchSize", "miniBatch");
 	private static List<String> BATCH_NORM_CONF_NAMES = Arrays.asList("batchNorm");
-	private static List<String> TEST_SPLIT_CONF_NAMES = Arrays.asList("testSplit","internalTestSplit");
-//	
+	private static List<String> TEST_FRAC_CONF_NAMES = Arrays.asList("testFrac","internalTestFrac");
 	private static List<String> EARLY_STOP_AFTER_CONF_NAMES = Arrays.asList("earlyStopAfter"); 
 	private static List<String> WEIGHT_DECAY_CONF_NAMES = Arrays.asList("weightDecay");
 	private static List<String> L2_CONF_NAMES = Arrays.asList("l2");
@@ -335,9 +334,9 @@ public abstract class DL4JMultiLayerBase
 		confs.add(new IntegerConfigParameter(BATCH_SIZE_CONF_NAMES, null)
 				.addDescription("The mini-batch size, will control how many records are passed in each iteration. The default is to use 10 mini-batches for each epoch "
 						+ "- thus calculated at training-time. Setting a value smaller than 0 disables mini-batches and passes _all data_ through at the same time (i.e., one batch per epoch)."));
-		confs.add(new BooleanConfigParameter(BATCH_NORM_CONF_NAMES, false)
+		confs.add(new BooleanConfigParameter(BATCH_NORM_CONF_NAMES, false, Arrays.asList(true,false))
 				.addDescription("Add batch normalization between all the dense layers"));
-		confs.add(new NumericConfigParameter(TEST_SPLIT_CONF_NAMES, DEFAULT_TEST_SPLIT_FRAC, Range.closed(0., .5))
+		confs.add(new NumericConfigParameter(TEST_FRAC_CONF_NAMES, DEFAULT_TEST_SPLIT_FRAC, Range.closed(0., .5))
 				.addDescription("Fraction of training examples that should be used for monitoring improvements of the network during training, these will not be used in the training of the network. If there are examples in this internal test-set, these will be used for determining early stopping - otherwise all training examples loss scores are used instead."));
 		confs.add(new IntegerConfigParameter(EARLY_STOP_AFTER_CONF_NAMES, DEFAULT_ES_N_EXTRA_EPOCH, Range.atLeast(1))
 				.addDescription("Determines how many epochs to continue to run without having an improvement in the loss function. If there should be no early stopping (always run all specified epochs) specify the same number as that of parameter "+N_EPOCH_CONF_NAMES.get(0)));		
@@ -345,9 +344,10 @@ public abstract class DL4JMultiLayerBase
 				.addDescription("The weight-decay regularization term, put to <=0 if not to use weight-decay regularization"));
 		confs.add(new NumericConfigParameter(L2_CONF_NAMES, 0).addDescription("L2 regularization term. Disabled by default."));
 		confs.add(new NumericConfigParameter(L1_CONF_NAMES, 0).addDescription("L1 regularization term. Disabled by default."));
-		confs.add(new EnumConfigParameter<>(LOSS_FUNC_CONF_NAMES, EnumSet.allOf(LossFunction.class),loss).addDescription("The loss function of the network"));
-		confs.add(new EnumConfigParameter<>(ACTIVATION_CONF_NAMES, EnumSet.allOf(Activation.class),Activation.RELU).addDescription("The activation function for the hidden layers"));
-
+		confs.add(new EnumConfigParameter<>(LOSS_FUNC_CONF_NAMES, EnumSet.allOf(LossFunction.class),loss)
+				.addDescription("The loss function of the network"));
+		confs.add(new EnumConfigParameter<>(ACTIVATION_CONF_NAMES, EnumSet.allOf(Activation.class),Activation.RELU)
+				.addDescription("The activation function for the hidden layers"));
 		return confs;
 	}
 
@@ -420,7 +420,7 @@ public abstract class DL4JMultiLayerBase
 					batchSize = TypeUtils.asInt(c.getValue());
 			} else if (CollectionUtils.containsIgnoreCase(BATCH_NORM_CONF_NAMES, key)) {
 				batchNorm = TypeUtils.asBoolean(c.getValue());
-			} else if (CollectionUtils.containsIgnoreCase(TEST_SPLIT_CONF_NAMES, key)) {
+			} else if (CollectionUtils.containsIgnoreCase(TEST_FRAC_CONF_NAMES, key)) {
 				setTestSplitFraction(TypeUtils.asDouble(c.getValue()));
 			} else if (CollectionUtils.containsIgnoreCase(EARLY_STOP_AFTER_CONF_NAMES, key)) {
 				setEarlyStopAfter(TypeUtils.asInt(c.getValue()));
