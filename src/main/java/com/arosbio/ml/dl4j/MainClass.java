529f5cebcb9098ec5b0c76d5a8d44ad8ed295367
staffan <staffan.arvidsson@gmail.com>
2021-11-17 08:23:21 +0100

    add main-class to print basic info about the JAR

---
+++ src/main/java/com/arosbio/ml/dl4j/MainClass.java
@@ -0,0 +1,9 @@
+package com.arosbio.ml.dl4j;
+
+public class MainClass {
+	
+	public static void main(String[] args) {
+		System.out.printf("%nThis JAR file is an extension to the CPSign program - providing support for Deeplearning based ML models.%nPlease visit https://github.com/arosbio/cpsign-dl4j for more information%n%n");
+	}
+
+}
