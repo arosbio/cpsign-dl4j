3e291093becbeb8b221873e872b02b4fb92f6542
Staffan Arvidsson <staffan.arvidsson@gmail.com>
2022-02-14 13:33:47 +0000

    added a CLI based test for seed

---
+++ src/test/cli_based/test_repeatable_cpu_threads.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+#-Dorg.bytedeco.javacpp.maxbytes=35G 
+# java -cp /home/jovyan/git/cpsign-dl4j/libs/cpsign:/home/jovyan/git/cpsign-dl4j/target/cpsign-dl4j.jar com.arosbio.modeling.app.cli.CPSignApp
+
+# Set parameters
+CLEAN_UP_AFTER=false
+PRECOMP_DS=../resources/data/class/precomputed.jar
+RUN_CMD="java -Xmx8G -Dorg.bytedeco.javacpp.maxphysicalbytes=30G -cp /home/jovyan/git/cpsign-dl4j/libs/cpsign:/home/jovyan/git/cpsign-dl4j/target/cpsign-dl4j.jar com.arosbio.modeling.app.cli.CPSignApp"
+LIC_PATH=../resources/licenses/cpsign2-develop-std.license
+SEED=56781214
+
+# First check
+$RUN_CMD -V
+
+#exit 0
+
+
+# DEFINE TEST FUNCTION TO CALL SEVERAL TIMES
+
+run_test(){
+    mkdir -p tmp/
+
+    # RUN CV, the results should hopefully change between runs
+    $RUN_CMD cv \
+        --seed $SEED \
+        --logfile "tmp/run_$1_log.txt" \
+        --license $LIC_PATH \
+        -ds $PRECOMP_DS \
+        --sampling-strategy Random \
+        --scorer DLClassifier:width=50:nEpoch=100 \
+        -rf csv \
+        --ncm ProbabilityMargin
+        
+
+}
+
+## Analyse CPU first
+export BACKEND_PRIORITY_CPU=10
+export BACKEND_PRIORITY_GPU=1
+
+
+# Round 1 - use the default settings
+run_test 1
+
+
+# Round 2 - 1 thread
+export OMP_NUM_THREADS=1
+run_test 2
+
+# Round 3 - 5 threads
+export OMP_NUM_THREADS=5
+run_test 3
+
+# Round 4 - 10 threads
+export OMP_NUM_THREADS=10
+run_test 4
+
+# Round 5 - 10 threads
+export OMP_NUM_THREADS=10
+run_test 4
+
+if [[ "$CLEAN_UP_AFTER" == true ]]; then
+    rm -rf tmp/
+fi
